3e681249721ace74456c15fd69046b97cb86790c
Remi Rivera <remirivera25@gmail.com>
2022-02-10 00:47:22 -0500

    Patch 1.0.3 - The Button Update

--- app/src/main/java/com/example/myyearbook/MainActivity.java
+++ app/src/main/java/com/example/myyearbook/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.myyearbook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.btnChange);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final TextView mTextView = (TextView) findViewById(R.id.textView);
+                mTextView.setText("Victini is a Fire/Psychic type Pokemon");
+            }
+        });
+
+
+
     }
 }
