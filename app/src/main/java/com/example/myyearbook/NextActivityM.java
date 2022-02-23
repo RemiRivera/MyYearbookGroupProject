57da8494e84f3f56ec1e35d09493e4e3164f146b
mriyaz2 <mriyaz2@student.sjcny.edu>
2022-02-23 17:09:49 -0500

    my changes

--- app/src/main/java/com/example/myyearbook/NextActivityM.java
+++ app/src/main/java/com/example/myyearbook/NextActivityM.java
@@ -4,35 +4,42 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.ActionBar;
+import android.content.Context;
+import android.net.Uri;
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class NextActivityM extends AppCompatActivity {
-
+    Button bt;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle SavedInstanceState) {
+        super.onCreate(SavedInstanceState);
         setContentView(R.layout.activity_next_m);
+
+        bt = (Button) findViewById(R.id.buttonShare);
+
+        bt.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+
+                Intent myIntent = new Intent(Intent.ACTION_SEND);
+                myIntent.setType("text/plain");
+                //This shared link sends the recipient to a webpage with all the information about Jiraiya
+                String body = "https://naruto.fandom.com/wiki/Jiraiya";
+                String sub = "All You Need to Know About Jiraiya";
+                myIntent.putExtra(Intent.EXTRA_SUBJECT, sub);
+                myIntent.putExtra(Intent.EXTRA_TEXT, body);
+                startActivity(Intent.createChooser(myIntent, "Share Using"));
+            }
+        });
     }
-
-
-    public void onClick(View view) {
-        Intent myIntent = new Intent(Intent.ACTION_SEND);
-        myIntent.setType("text/plain");
-        String body = "Your body here";
-        String sub = "Your Subject";
-        myIntent.putExtra(Intent.EXTRA_SUBJECT, sub);
-        myIntent.putExtra(Intent.EXTRA_TEXT, body);
-        startActivity(Intent.createChooser(myIntent, "Share Using"));
-    }
+}
 
 
 
-    }
+
     
 
 
