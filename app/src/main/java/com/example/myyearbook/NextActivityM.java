b5c9543ab02b879ba80cbdca143cf63b5a80eb0a
mriyaz2 <mriyaz2@student.sjcny.edu>
2022-02-23 15:41:30 -0500

    my changes

--- app/src/main/java/com/example/myyearbook/NextActivityM.java
+++ app/src/main/java/com/example/myyearbook/NextActivityM.java
@@ -1,22 +1,59 @@
 package com.example.myyearbook;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+
 public class NextActivityM extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next_m);
     }
-            public void onClick(View view){
-            Intent i = new Intent(getApplicationContext(), Dashboard.class);
-            startActivity(i);
-        }
 
-}
+
+    public void onClick(View view) {
+        Intent myIntent = new Intent(Intent.ACTION_SEND);
+        myIntent.setType("text/plain");
+        String body = "Your body here";
+        String sub = "Your Subject";
+        myIntent.putExtra(Intent.EXTRA_SUBJECT, sub);
+        myIntent.putExtra(Intent.EXTRA_TEXT, body);
+        startActivity(Intent.createChooser(myIntent, "Share Using"));
+    }
+
+
+
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
