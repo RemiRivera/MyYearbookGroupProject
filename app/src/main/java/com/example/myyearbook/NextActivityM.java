4d5d5dc5bce08d1fa00c7393dbe465f861ddbd8d
mriyaz2 <mriyaz2@student.sjcny.edu>
2022-02-23 17:10:16 -0500

    my changes

--- app/src/main/java/com/example/myyearbook/NextActivityM.java
+++ app/src/main/java/com/example/myyearbook/NextActivityM.java
@@ -4,8 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.content.Context;
-import android.net.Uri;
 import androidx.appcompat.app.AppCompatActivity;
 
 
